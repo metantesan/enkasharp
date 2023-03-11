2282a3292ffc4bbed513e00f47516e81b907eb2f
metantesan <mps.parsa.mps@gmail.com>
2023-03-11 19:05:43 +0330

    try new example

--- EnkaSharpExampel/Program.cs
+++ EnkaSharpExampel/Program.cs
@@ -1,6 +1,8 @@
-﻿using EnkaSharp;
-using EnkaSharp.Repository;
+﻿using EnkaSharp.Repository;
 using EnkaSharp.Service;
 
 IEnkaRepository enka=new EnkaRepository("en");
-await enka.Info(743547342);
+var a = await enka.Info(743547342);
+Console.WriteLine(a.Owner.Profile.Avatar);
+
+
